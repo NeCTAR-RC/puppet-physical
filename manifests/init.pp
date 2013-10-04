53a824a34943c103da3b1e5aaec93156cb9e2722
Marcus Furlong <furlongm@gmail.com>
2013-10-04 14:24:12 +1000

    add mcelog and edac error reporting
    Change-Id: I14a2245949c2a3e3aaed659f71ee4ac802c91148

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,34 @@ class physical {
     ensure => installed,
   }
 
+  package { ['edac-utils', 'mcelog']:
+    ensure => installed,
+  }
+
+  file { '/usr/local/lib/nagios/plugins/check_edac':
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/physical/check_edac',
+    require => Package['edac-utils'],
+  }
+
+  nagios::nrpe::service { 'check_edac':
+     check_command => '/usr/local/lib/nagios/plugins/check_edac';
+  }
+
+  file { '/usr/local/lib/nagios/plugins/check_mcelog':
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/physical/check_mcelog',
+    require => Package['mcelog'],
+  }
+
+  nagios::nrpe::service { 'check_mcelog':
+     check_command => '/usr/local/lib/nagios/plugins/check_mcelog';
+  }
+
   case $::manufacturer {
 
     'HP' :         { include physical::hp }
