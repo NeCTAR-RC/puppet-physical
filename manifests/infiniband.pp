01531bff87a2a13dbecb246d0aeab2e322ece8a6
Marcus Furlong <furlongm@gmail.com>
2013-12-18 17:52:29 +1100

    updated infiniband module for 3.2 kernels
    Change-Id: I2ef0bf138ca092f1dde2ff00d4bf3bc9fbbe7a92

--- manifests/infiniband.pp
+++ manifests/infiniband.pp
@@ -1,17 +1,20 @@
 class physical::infiniband {
 
   file { '/etc/modprobe.d/mlx4.conf':
-    ensure  => present,
-    content => 'install mlx4_core /sbin/modprobe --ignore-install mlx4_core; /sbin/modprobe mlx4_en',
+    ensure  => absent,
   }
 
   package { 'mlx4-dkms':
+    ensure => absent,
+  }
+
+  package { 'mlx4-en-dkms':
     ensure => present,
   }
 
-  exec { 'update-initramfs-mlx' :
+  exec { 'update-initramfs-mlx':
     command     => '/usr/sbin/update-initramfs -k all -u',
-    subscribe   => [File['/etc/modprobe.d/mlx4.conf'], Package['mlx4-dkms']],
+    subscribe   => Package['mlnx4-en-dkms'],
     refreshonly => true,
   }
 }
