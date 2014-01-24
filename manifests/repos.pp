b6727ab103b0e3880a2bfcb05228b2eaf47fe4d4
Marcus Furlong <furlongm@gmail.com>
2014-01-24 16:09:38 +1100

    add hwraid repo
    Change-Id: I078706fe25921ccec5106865675524520ef4ad41

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -1,15 +1,43 @@
-class physical::packages {
+class physical::repos {
 
   case $::manufacturer {
 
-    'HP' :         { include physical::packages::hp }
-    'Dell Inc.' :  { include physical::packages::dell }
-    'Supermicro' : { include physical::packages::supermicro }
+    'HP' :         { include physical::repos::hp }
+    'Dell Inc.' :  { include physical::repos::dell }
+    'Supermicro' : { include physical::repos::supermicro }
 
+  }
+
+  include physical::repos::hwraid
+
+}
+
+class physical::repos::hwraid {
+
+  if $::rfc1918_gateway == 'true' {
+    exec { 'hwraid-apt-key':
+     path        => '/usr/bin:/bin:/usr/sbin:/sbin',
+     command     => "apt-key adv --keyserver pgp.mit.edu --keyserver-options http-proxy=\"${::http_proxy}\" --recv-keys 34A9CF8E",
+     unless      => 'apt-key list | grep 34A9CF8E >/dev/null 2>&1',
+    }
+
+  } else {
+    apt::key { 'hwraid':
+      key        => '34A9CF8E',
+      key_server => 'pgp.mit.edu',
+    }
+  }
+
+  apt::source { 'hwraid':
+    location    => 'http://hwraid.le-vert.net/ubuntu',
+    release     => 'precise',
+    repos       => 'main',
+    key         => '34A9CF8E',
+    include_src => false,
   }
 }
 
-class physical::packages::hp {
+class physical::repos::hp {
 
   if $::rfc1918_gateway == 'true' {
     exec { 'hp-apt-key':
@@ -34,7 +62,7 @@ class physical::packages::hp {
   }
 }
 
-class physical::packages::dell {
+class physical::repos::dell {
 
   if $::rfc1918_gateway == 'true' {
     exec { 'dell-apt-key':
@@ -59,6 +87,6 @@ class physical::packages::dell {
   }
 }
 
-class physical::packages::supermicro {
+class physical::repos::supermicro {
 
 }
