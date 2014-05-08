451364779ca8b79e7472a4c4012226dc4cb2a2d7
Marcus Furlong <furlongm@gmail.com>
2014-05-08 13:42:15 +1000

    switch to check_raid command for use with trusty
    Change-Id: I66ee12f5add005076cd0d71eec3ce126788a102c

--- manifests/mdraid.pp
+++ manifests/mdraid.pp
@@ -1,7 +1,7 @@
 class physical::mdraid {
 
   nagios::nrpe::service { 'check_md_raid':
-    check_command => '/usr/lib/nagios/plugins/check_linux_raid';
+    check_command => '/usr/lib/nagios/plugins/check_raid';
   }
 
 }
