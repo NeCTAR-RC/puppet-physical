45c8af353887956fec12e334f75e57423bc14d8c
Marcus Furlong <furlongm@gmail.com>
2014-05-22 17:56:25 +1000

    nagios needs sudo powers to check the raid status
    Change-Id: Iddf172c8b8392e23bd205bb1f0cf48ce8302b1bf

--- manifests/mdraid.pp
+++ manifests/mdraid.pp
@@ -4,4 +4,10 @@ class physical::mdraid {
     check_command => '/usr/lib/nagios/plugins/check_raid -p lsscsi,mdstat,megacli';
   }
 
+  file { '/etc/sudoers.d/nagios_raid':
+    owner   => root,
+    group   => root,
+    mode    => '0440',
+    source  => 'puppet:///modules/physical/sudoers_nagios_raid',
+  }
 }
