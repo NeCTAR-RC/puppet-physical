ebbf7ed25e675d3069b292aa5c6d2d9eb657294c
Sam Morrison <sorrison@gmail.com>
2014-06-19 09:15:08 +1000

    Fix apt key download behind proxy
    Change-Id: I292042b1be5d66a70e0014dd857b6bc3826d641f

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -14,48 +14,69 @@ class physical::repo {
 
 class physical::repo::hwraid {
 
+  if $::http_proxy and $::rfc1918_gateway == 'true' {
+    $key_options = "http-proxy=${::http_proxy}"
+  }
+  else {
+    $key_options = false
+  }
+
   apt::key { 'hwraid':
     key        => '34A9CF8E',
     key_server => 'pgp.mit.edu',
+    key_options => $key_options
   }
 
   apt::source { 'hwraid':
     location    => 'http://hwraid.le-vert.net/ubuntu',
     release     => 'precise',
     repos       => 'main',
-    key         => '34A9CF8E',
     include_src => false,
   }
 }
 
 class physical::repo::hp {
 
+  if $::http_proxy and $::rfc1918_gateway == 'true' {
+    $key_options = "http-proxy=${::http_proxy}"
+  }
+  else {
+    $key_options = false
+  }
+
   apt::key { 'hp':
     key        => '57E5E96D',
     key_server => 'pgp.mit.edu',
+    key_options => $key_options
   }
 
   apt::source { 'hp':
     location    => 'http://downloads.linux.hp.com/SDR/downloads/MCP/ubuntu',
     release     => 'precise/current',
     repos       => 'non-free',
-    key         => '57E5E96D',
     include_src => false,
   }
 }
 
 class physical::repo::dell {
 
+  if $::http_proxy and $::rfc1918_gateway == 'true' {
+    $key_options = "http-proxy=${::http_proxy}"
+  }
+  else {
+    $key_options = false
+  }
+
   apt::key { 'dell':
     key        => '34D8786F',
     key_server => 'pgp.mit.edu',
+    key_options => $key_options
   }
 
   apt::source { 'dell':
     location    => 'http://linux.dell.com/repo/community/deb/latest ',
     release     => '/',
     repos       => '',
-    key         => '34D8786F',
     include_src => false,
   }
 }
