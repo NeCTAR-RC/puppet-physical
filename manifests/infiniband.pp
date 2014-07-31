225ed9ae2c89198b61a51443f848fe5fa3d55412
Sam Morrison <sorrison@gmail.com>
2014-07-31 15:40:07 +1000

    Infinband module only supported on precise for now
    Change-Id: Iff2ae8a9bbbc4eec8d80e0d0c04dde574f3b08e4

--- manifests/infiniband.pp
+++ manifests/infiniband.pp
@@ -1,20 +1,23 @@
 class physical::infiniband {
 
-  file { '/etc/modprobe.d/mlx4.conf':
-    ensure  => absent,
-  }
-
-  package { 'mlx4-dkms':
-    ensure => absent,
-  }
-
-  package { 'mlnx-en-dkms':
-    ensure => present,
-  }
-
-  exec { 'update-initramfs-mlnx':
-    command     => '/usr/sbin/update-initramfs -k all -u',
-    subscribe   => Package['mlnx-en-dkms'],
-    refreshonly => true,
+  if $::lsbdistcodename == 'precise' {
+  
+    file { '/etc/modprobe.d/mlx4.conf':
+      ensure  => absent,
+    }
+    
+    package { 'mlx4-dkms':
+      ensure => absent,
+    }
+    
+    package { 'mlnx-en-dkms':
+      ensure => present,
+    }
+    
+    exec { 'update-initramfs-mlnx':
+      command     => '/usr/sbin/update-initramfs -k all -u',
+      subscribe   => Package['mlnx-en-dkms'],
+      refreshonly => true,
+    }
   }
 }
