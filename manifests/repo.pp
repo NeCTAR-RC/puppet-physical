662c6111e0d47f0b44b4a0e8ca7e7b3c426623f5
Sam Morrison <sorrison@gmail.com>
2016-12-09 10:07:55 +1100

    Remove deprecated options
    Change-Id: Iaaae19175dd00714b7abd7f4f35dffacf694eef0

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -33,62 +33,59 @@ class physical::repo::hwraid {
   }
 
   apt::key { 'hwraid':
-    key        => '0073C11919A641464163F7116005210E23B3D3B4',
-    key_server => 'pgp.mit.edu',
-    key_options => $key_options
+    id      => '0073C11919A641464163F7116005210E23B3D3B4',
+    server  => 'pgp.mit.edu',
+    options => $key_options
   }
 
   apt::source { 'hwraid':
     location    => 'http://hwraid.le-vert.net/ubuntu',
     release     => 'precise',
     repos       => 'main',
-    include_src => false,
   }
 }
 
 class physical::repo::hp {
 
   if $::http_proxy and $::rfc1918_gateway == 'true' {
-    $key_options = "http-proxy=${::http_proxy}"
+    $options = "http-proxy=${::http_proxy}"
   }
   else {
-    $key_options = false
+    $options = false
   }
 
   apt::key { 'hp':
-    key         => '882F7199B20F94BD7E3E690EFADD8D64B1275EA3',
-    key_server  => 'pgp.mit.edu',
-    key_options => $key_options
+    id      => '882F7199B20F94BD7E3E690EFADD8D64B1275EA3',
+    server  => 'pgp.mit.edu',
+    options => $options
   }
 
   apt::source { 'hp':
     location    => 'http://downloads.linux.hpe.com/SDR/downloads/MCP/ubuntu',
     release     => "${::lsbdistcodename}/current",
     repos       => 'non-free',
-    include_src => false,
   }
 }
 
 class physical::repo::dell {
 
   if $::http_proxy and $::rfc1918_gateway == 'true' {
-    $key_options = "http-proxy=${::http_proxy}"
+    $options = "http-proxy=${::http_proxy}"
   }
   else {
-    $key_options = false
+    $options = false
   }
 
   apt::key { 'dell':
-    key        => '42550ABD1E80D7C1BC0BAD851285491434D8786F',
-    key_server => 'pgp.mit.edu',
-    key_options => $key_options
+    id      => '42550ABD1E80D7C1BC0BAD851285491434D8786F',
+    server  => 'pgp.mit.edu',
+    options => $options
   }
 
   apt::source { 'dell':
     location    => 'http://linux.dell.com/repo/community/deb/latest ',
     release     => '/',
     repos       => '',
-    include_src => false,
   }
 }
 
