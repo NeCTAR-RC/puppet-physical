4abb76e56c10b12f1c304bff10db6ca50c7e6b1a
Jake Yip <waipengyip@gmail.com>
2017-02-21 18:00:38 +1100

    Stringify facts for puppet4
    Change-Id: I49689578bfe82120c10d3e4f3a917d0f43361871

--- manifests/supermicro.pp
+++ manifests/supermicro.pp
@@ -1,8 +1,9 @@
+# class for supermicro boxes
 class physical::supermicro {
 
   if $::productname == 'H8DGT' {
 
-    if $h8dgt_ps1 == 'true' and $h8dgt_pm_ps1 == 'true' {
+    if str2bool($::h8dgt_ps1) and str2bool($::h8dgt_pm_ps1) {
 
       nagios::nrpe::service  { 'check_powersupply1':
         check_command => '/usr/local/lib/nagios/plugins/check_powersupply 1',
@@ -10,7 +11,7 @@ class physical::supermicro {
       }
     }
 
-    if $h8dgt_ps2 == 'true' and $h8dgt_pm_ps2 == 'true' {
+    if str2bool($::h8dgt_ps2) and str2bool($::h8dgt_pm_ps2) {
 
       nagios::nrpe::service  { 'check_powersupply2':
         check_command => '/usr/local/lib/nagios/plugins/check_powersupply 2',
@@ -18,7 +19,7 @@ class physical::supermicro {
       }
     }
 
-    if $h8dgt_ps3 == 'true' and $h8dgt_pm_ps2 == 'true' {
+    if str2bool($::h8dgt_ps3) and str2bool($::h8dgt_pm_ps2) {
 
       nagios::nrpe::service  { 'check_powersupply3':
         check_command => '/usr/local/lib/nagios/plugins/check_powersupply 3',
