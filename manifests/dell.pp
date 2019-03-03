0c8f525d062e39e09a6a9f10812b48b8c682fc18
Sam Morrison <sorrison@gmail.com>
2019-03-04 10:07:08 +1100

    Install required lbxslt for dell (also fix indents)
    Change-Id: Id6db1d167402af572dcd9d8582cb2318934d8823

--- manifests/dell.pp
+++ manifests/dell.pp
@@ -2,60 +2,60 @@ class physical::dell (
   $openmanage_check_args='--no-storage',
 ){
 
-    include physical::ipmi::kern_modules
+  include physical::ipmi::kern_modules
 
-    file { '/etc/sudoers.d/nagios_dell':
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0440',
-      source => 'puppet:///modules/physical/sudoers_nagios_dell',
-    }
+  file { '/etc/sudoers.d/nagios_dell':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0440',
+    source => 'puppet:///modules/physical/sudoers_nagios_dell',
+  }
 
-    package { ['srvadmin-base', 'srvadmin-omcommon', 'srvadmin-omacore']:
-      ensure => present,
-      tag    => 'dell',
-    }
+  package { ['srvadmin-base', 'srvadmin-omcommon', 'srvadmin-omacore', 'libxslt1.1']:
+    ensure => present,
+    tag    => 'dell',
+  }
 
-    file { '/usr/local/lib/nagios/plugins/check_openmanage':
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0755',
-      source => 'puppet:///modules/physical/check_openmanage',
-    }
+  file { '/usr/local/lib/nagios/plugins/check_openmanage':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/physical/check_openmanage',
+  }
 
-    file { '/opt/dell/srvadmin/var/log/openmanage/omcmdlog.xml':
-      ensure  => link,
-      target  => '/var/log/omdell.xml',
-      owner   => 'nagios',
-      group   => 'nagios',
-      require => Package['srvadmin-base'],
-    }
+  file { '/opt/dell/srvadmin/var/log/openmanage/omcmdlog.xml':
+    ensure  => link,
+    target  => '/var/log/omdell.xml',
+    owner   => 'nagios',
+    group   => 'nagios',
+    require => Package['srvadmin-base'],libxslt1.1
+  }
 
-    file { '/var/log/omdell.xml':
-      ensure => present,
-      owner  => 'nagios',
-      group  => 'nagios',
-      mode   => '1664'
-    }
-    if versioncmp($::operatingsystemrelease, '18.04') < 0 { # pre-bionic
-      service { 'dataeng':
-        ensure  => running,
-        enable  => true,
-        require => [Package['srvadmin-base'],
+  file { '/var/log/omdell.xml':
+    ensure => present,
+    owner  => 'nagios',
+    group  => 'nagios',
+    mode   => '1664'
+  }
+  if versioncmp($::operatingsystemrelease, '18.04') < 0 {
+    # pre-bionic
+    service { 'dataeng':
+      ensure  => running,
+      enable  => true,
+      require => [Package['srvadmin-base'],
                   Package['srvadmin-omcommon'],
                   Puppet::Kern_module['ipmi_devintf'],
                   Puppet::Kern_module['ipmi_si'],],
-      }
     }
-    else { # bionic and newer with OM 9+ don't have /etc/init.d/dataeng
-      service { ['dsm_sa_datamgrd.service', 'dsm_sa_eventmgrd.service', ]:
-        ensure  => 'running',
-        enable  => true,
-        require => [Package['srvadmin-base'], Package['srvadmin-omacore']],
-      }
+  } else { # bionic and newer with OM 9+ don't have /etc/init.d/dataeng
+    service { ['dsm_sa_datamgrd.service', 'dsm_sa_eventmgrd.service', ]:
+      ensure  => 'running',
+      enable  => true,
+      require => [Package['srvadmin-base'], Package['srvadmin-omacore']],
     }
-    nagios::nrpe::service { 'check_openmanage':
-      check_command => "/usr/bin/sudo /usr/local/lib/nagios/plugins/check_openmanage ${openmanage_check_args}",
-      nrpe_command  => 'check_nrpe_slow_1arg'
-    }
+  }
+  nagios::nrpe::service { 'check_openmanage':
+    check_command => "/usr/bin/sudo /usr/local/lib/nagios/plugins/check_openmanage ${openmanage_check_args}",
+    nrpe_command  => 'check_nrpe_slow_1arg'
+  }
 }
