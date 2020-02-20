f79b1cd01ff7554c1d6ae587660f7532b061daa5
Karl Goetz <karl.goetz@tpac.org.au>
2020-02-20 11:14:46 +1100

    Add script to check for read only mounts
    Read only filesystems are often a warning that raid arrays (or disks in
    general) have gone bad.
    We've had several of them lately and it would be great to receive advance
    notice of future occurences.
    
    Change-Id: Ic9fee4c18286e4641453f1ce9e7728ce43bfd988

---
+++ manifests/check_ro_mounts.pp
@@ -0,0 +1,16 @@
+# Check if a server has read only filesystems as cheap proxy to disk failure
+class physical::check_ro_mounts($ensure='present', $extra_options=undef) {
+
+  file { '/usr/local/lib/nagios/plugins/check_ro_mounts':
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    source  => 'puppet:///modules/physical/check_ro_mounts',
+  }
+
+  nagios::nrpe::service  { 'check_ro_mounts':
+    check_command => '/usr/local/lib/nagios/plugins/check_ro_mounts -X sysfs -X tmpfs -X cgroup' $extra_options,
+    nrpe_command  => 'check_ro_mounts',
+  }
+
+}
