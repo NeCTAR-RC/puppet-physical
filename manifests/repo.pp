76cb004e3f9b6c8557101ab49fa357839d6f8a85
Linh Vu <vul@unimelb.edu.au>
2020-05-19 16:51:31 +1000

    allow override dell repo url for local mirror
    had to split to physical/manifests/repo/dell.pp else this would happen:
    
    https://jenkins.rc.nectar.org.au/job/puppet-physical-puppet-unit/109/console
    
    "manifests/repo.pp - ERROR: physical::repo::dell not in autoload module layout on line 59"
    
    Change-Id: If48b12aab5a52359b2da0cc02e9ddc254153fa52

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -55,39 +55,3 @@ class physical::repo::hp {
     require  => Apt::Key['hp'],
   }
 }
-
-class physical::repo::dell {
-
-  if defined('$::http_proxy') and str2bool($::rfc1918_gateway) {
-    $key_options = "http-proxy=${::http_proxy}"
-  }
-  else {
-    $key_options = undef
-  }
-
-  apt::key { 'dell':
-    id      => '42550ABD1E80D7C1BC0BAD851285491434D8786F',
-    server  => 'pool.sks-keyservers.net',
-    options => $key_options
-  }
-
-  # follow the guide on https://linux.dell.com/repo/community/openmanage/
-  if versioncmp($::operatingsystemrelease, '18.04') < 0 { # pre-bionic
-    apt::source { 'dell':
-      location => 'http://linux.dell.com/repo/community/ubuntu',
-      release  => $::lsbdistcodename,
-      repos    => 'openmanage',
-    }
-  }
-  else { # bionic and later
-    apt::source { 'dell':
-      location => "http://linux.dell.com/repo/community/openmanage/930/${::lsbdistcodename}",
-      release  => $::lsbdistcodename,
-      repos    => 'main',
-      require  => Apt::Key['dell'],
-    }
-  }
-
-  Apt::Source <| title == 'dell' |> -> Class['apt::update'] -> Package <| tag == 'dell' |>
-
-}
---
+++ manifests/repo/dell.pp
@@ -0,0 +1,38 @@
+# dell openmanage repo
+class physical::repo::dell(
+  $mirror_url = "http://linux.dell.com/repo/community/openmanage/930/${::lsbdistcodename}",
+) {
+
+  if defined('$::http_proxy') and str2bool($::rfc1918_gateway) {
+    $key_options = "http-proxy=${::http_proxy}"
+  }
+  else {
+    $key_options = undef
+  }
+
+  apt::key { 'dell':
+    id      => '42550ABD1E80D7C1BC0BAD851285491434D8786F',
+    server  => 'pool.sks-keyservers.net',
+    options => $key_options
+  }
+
+  # follow the guide on https://linux.dell.com/repo/community/openmanage/
+  if versioncmp($::operatingsystemrelease, '18.04') < 0 { # pre-bionic
+    apt::source { 'dell':
+      location => 'http://linux.dell.com/repo/community/ubuntu',
+      release  => $::lsbdistcodename,
+      repos    => 'openmanage',
+    }
+  }
+  else { # bionic and later
+    apt::source { 'dell':
+      location => $mirror_url,
+      release  => $::lsbdistcodename,
+      repos    => 'main',
+      require  => Apt::Key['dell'],
+    }
+  }
+
+  Apt::Source <| title == 'dell' |> -> Class['apt::update'] -> Package <| tag == 'dell' |>
+
+}
