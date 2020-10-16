652e2b6c74a0862e8d18c7ab810dca9b4e10141a
Sam Morrison <sorrison@gmail.com>
2020-10-16 14:40:30 +1100

    Set some defaults for dell repos
    Change-Id: I86a91578f716faf8da317700d5dc445a9a9ffe3e

--- manifests/repo/dell.pp
+++ manifests/repo/dell.pp
@@ -1,6 +1,6 @@
 # dell openmanage repo
 class physical::repo::dell(
-  $mirror_url = 'http://linux.dell.com/repo/community/openmanage/930/bionic',
+  String $mirror_url,
 ) {
 
   if defined('$::http_proxy') and str2bool($::rfc1918_gateway) {
@@ -24,10 +24,10 @@ class physical::repo::dell(
       repos    => 'openmanage',
     }
   }
-  else { # bionic and later (currently only bionic repo available)
+  else { # bionic and later
     apt::source { 'dell':
       location => $mirror_url,
-      release  => 'bionic',
+      release  => $::lsbdistcodename,
       repos    => 'main',
       require  => Apt::Key['dell'],
     }
