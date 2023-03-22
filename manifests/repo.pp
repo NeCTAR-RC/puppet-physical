4c81d8ef0af53358257a9bb724d74108a30b8276
Sam Morrison <sorrison@gmail.com>
2023-03-22 13:59:50 +1100

    Tidy up repo classes
    Change-Id: Id92d1f8e4308fc3edcc0236c5394b77bc134507d

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,4 +1,6 @@
-class physical::repo( $hwraid = true, ) {
+class physical::repo (
+  Boolean $hwraid = true,
+) {
 
   case $::manufacturer {
     'HP' :         { require ::physical::repo::hp }
@@ -7,28 +9,5 @@ class physical::repo( $hwraid = true, ) {
 
   if $hwraid {
     require ::physical::repo::hwraid
-  }
-}
-
-class physical::repo::hp {
-
-  if defined('$::http_proxy') and str2bool($::rfc1918_gateway) {
-    $key_options = "http-proxy=${::http_proxy}"
-  }
-  else {
-    $key_options = undef
-  }
-
-  apt::key { 'hp':
-    id      => '882F7199B20F94BD7E3E690EFADD8D64B1275EA3',
-    server  => 'pool.sks-keyservers.net',
-    options => $key_options
-  }
-
-  apt::source { 'hp':
-    location => 'http://downloads.linux.hpe.com/SDR/downloads/MCP/ubuntu',
-    release  => "${facts['os']['distro']['codename']}/current",
-    repos    => 'non-free',
-    require  => Apt::Key['hp'],
   }
 }
