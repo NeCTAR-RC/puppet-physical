9a8d85389eb649f2526b81c48721d4b49d7c662b
Dylan McCulloch <dylan.mcculloch@gmail.com>
2024-06-18 16:36:05 +1000

    Revert "Drop support for bionic and less with dell class"
    This reverts commit 0c5f28dd2a81cf581ca30f33b5f895477e03dcc1.
    
    Reason for revert: Swinburne still running xenial hosts. Keep until they deploy new control plane.
    
    Change-Id: Ibdc30b43411fb58c8000209564b7f0cd3248841d

--- manifests/dell.pp
+++ manifests/dell.pp
@@ -38,13 +38,23 @@ class physical::dell (
     group  => 'nagios',
     mode   => '1664'
   }
-
-  service { ['dsm_sa_datamgrd.service', 'dsm_sa_eventmgrd.service', ]:
-    ensure  => 'running',
-    enable  => true,
-    require => [Package['srvadmin-base'], Package['srvadmin-omacore']],
+  if versioncmp($facts['os']['release']['full'], '18.04') < 0 {
+    # pre-bionic
+    service { 'dataeng':
+      ensure  => running,
+      enable  => true,
+      require => [Package['srvadmin-base'],
+                  Package['srvadmin-omcommon'],
+                  Puppet::Kern_module['ipmi_devintf'],
+                  Puppet::Kern_module['ipmi_si'],],
+    }
+  } else { # bionic and newer with OM 9+ don't have /etc/init.d/dataeng
+    service { ['dsm_sa_datamgrd.service', 'dsm_sa_eventmgrd.service', ]:
+      ensure  => 'running',
+      enable  => true,
+      require => [Package['srvadmin-base'], Package['srvadmin-omacore']],
+    }
   }
-
   nagios::nrpe::service { 'check_openmanage':
     check_command  => "/usr/bin/sudo /usr/local/lib/nagios/plugins/check_openmanage ${openmanage_check_args}",
     nrpe_command   => 'check_nrpe_slow_1arg',
